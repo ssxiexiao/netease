883b2261d08c6118df050fc86a8a89ebce0b6155
xiexiao <softwarexiexiao@gmail.com>
2016-02-24 22:33:25 +0800

    ...

---
+++ js/directive.js
@@ -0,0 +1,19 @@
+/*
+data description
+{
+  'core': [player1, player2...],
+  'common': [player1, player2...],
+  'attributesName': ['attr1', 'attr2'...],
+  'goodsName': [...]
+}
+player description
+{
+  'consumption': [ [], []... ],
+  'attributesValue': {
+    'attr1': [...],
+    'attr2': [...],
+    ...
+  },
+  'influence':[]
+}
+*/
